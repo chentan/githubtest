a74bb6d0d5e72959bcd6cbcf80e4bb4d126be860
chentan <smiling0510@gmail.com>
2020-08-12 16:29:01 -0400

    Update main.m

--- main.m
+++ main.m
@@ -5,3 +5,6 @@ for i = 1:100
     sum = sum + i;
 end
 disp(sum);
+
+% now I edit file in the server, and want to get the changes from the remote repository
+disp('pull changes from a remote repository')
