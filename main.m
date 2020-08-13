271de1478a6cc2f6ed6288fbb8e41bcaf434b2e6
chentan <smiling0510@gmail.com>
2020-08-12 22:14:57 -0400

    Revert "Add new feature"
    This reverts commit afc50885c551e12daff9f6f1db9cfb05de9d7b29.

--- main.m
+++ main.m
@@ -8,6 +8,3 @@ disp(sum);
 
 % now I edit file in the server, and want to get the changes from the remote repository
 disp('pull changes from a remote repository')
-
-% something I might not like and want to go back to history file
-disp('nah')
